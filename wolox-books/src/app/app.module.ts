0c7a413ba12d91016d0ef84b538f977dfa531fa6
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-27 10:51:04 -0500

    use app-user tag to import views

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserModule } from './screens/user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
