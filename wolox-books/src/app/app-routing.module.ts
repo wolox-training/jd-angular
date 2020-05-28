f2a7e73e6e956e8ed03b64008c343d22d11b94f1
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-28 14:37:55 -0500

    Add routes

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { UserComponent } from './screens/user/user.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'login', component: UserComponent },
+  { path: 'sign-up', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
