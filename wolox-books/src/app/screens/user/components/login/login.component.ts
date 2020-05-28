4ecf7241cfdbd29efc3a568b159d7d1c382aa95e
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-28 15:26:01 -0500

    Add formGroup, formBuilder and request to login

--- wolox-books/src/app/screens/user/components/login/login.component.ts
+++ wolox-books/src/app/screens/user/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(private readonly formBuilder: FormBuilder, private userService: UserService) {
+    this.formGroup = formBuilder.group({
+      'email': [null, [Validators.required, Validators.email]],
+      'password': [null, [Validators.required, Validators.pattern('(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}')]]
+    });
+  }
+
+  get email() {
+    return this.formGroup.controls.email;
+  }
+
+  get password() {
+    return this.formGroup.controls.password;
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.userService.login(this.formGroup.value).subscribe(
+      (response: HttpResponse<Object>) => {
+        console.log(response.body['access_token']);
+      },
+      (error) => {
+        console.error(`status: ${error.status}, error: ${error.message}`);
+      }
+    )    
   }
 
 }
