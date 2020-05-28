afa40c82b84467349d67d73ada8c8207f4bce07b
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-28 10:23:22 -0500

    Add HttpResponse object as a response type object

--- wolox-books/src/app/screens/user/user.service.ts
+++ wolox-books/src/app/screens/user/user.service.ts
@@ -1,6 +1,6 @@
 import { User } from './user';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,8 +12,8 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  add(user: User): Observable<Object> {
+  add(user: User): Observable<HttpResponse<Object>> {
     const data = Object.assign({ user: {...user, locale: 'en'} });
-    return this.httpClient.post(`${this.URL}/api/v1/users`, data);
+    return this.httpClient.post(`${this.URL}/api/v1/users`, data, {observe: 'response'});
   }
 }
