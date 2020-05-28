1850979ba66115b2375a914098b2463eba5676eb
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-28 07:56:54 -0500

    User data interface

---
+++ wolox-books/src/app/screens/user/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    first_name: string;
+
+    last_name: string;
+
+    email: string;
+
+    password: string;
+
+    password_confirmation: string;
+
+    locale: string;
+}
