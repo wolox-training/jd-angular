278248c3c44b7292f2d399f89c739b4ec663d841
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-28 07:57:27 -0500

    make user service request

---
+++ wolox-books/src/app/screens/user/user.service.ts
@@ -0,0 +1,19 @@
+import { User } from './user';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private URL: string = "http://wbooks-api-stage.herokuapp.com"
+
+  constructor(private httpClient: HttpClient) { }
+
+  add(user: User): Observable<Object> {
+    const data = Object.assign({ user: {...user, locale: 'en'} });
+    return this.httpClient.post(`${this.URL}/api/v1/users`, data);
+  }
+}
