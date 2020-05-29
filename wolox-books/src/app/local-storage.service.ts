75e3faa87d4611870caaafe0613da59e417912bd
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-29 11:11:29 -0500

    local storage service

---
+++ wolox-books/src/app/local-storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  private TOKEN_KEY: string = 'token';
+
+  constructor() { }
+
+  createSession(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  hasSession(): boolean {
+    let token = null;
+
+    if (localStorage.length > 0) {
+      token = localStorage.getItem(this.TOKEN_KEY);
+    }
+
+    return !!token;
+  }
+
+  deleteSession(): void {
+    localStorage.removeItem(this.TOKEN_KEY)
+  }
+}
