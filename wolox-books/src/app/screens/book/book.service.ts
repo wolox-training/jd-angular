55384ab66ab616b4deeea4ada1af95cc4ea7e834
Jorge Diaz <jorge.diaz@wolox.co>
2020-06-10 18:03:32 -0500

    Make request to list books

---
+++ wolox-books/src/app/screens/book/book.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private URL: string = "http://private-anon-6605de9515-wbooksapi.apiary-mock.com"
+
+  constructor(private httpClient: HttpClient) { }
+
+  list(): Observable<HttpResponse<Object>> {
+    return this.httpClient.get(
+      `${this.URL}/api/v1/booksClassic`,
+      { observe: 'response' }
+    );
+  }
+}
