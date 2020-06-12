d3409df7542fbc2db14f80f9fd011d7c3e1ccb37
Jorge Diaz <jorge.diaz@wolox.co>
2020-06-11 19:34:07 -0500

    Create book effect to send HTTP Request

---
+++ wolox-books/src/app/store/book.effect.ts
@@ -0,0 +1,21 @@
+import { BookService } from './../screens/book/book.service';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import * as BookActions from '../store/book.action';
+import { of, EMPTY } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BookEffect {
+  constructor(private actions: Actions, private bookService: BookService) {}
+
+  loadBooks = createEffect(() => this.actions.pipe(
+    ofType(BookActions.LIST_BOOK),
+    mergeMap(() => this.bookService.list()
+      .pipe(
+        map(books => new BookActions.ListedBook(books.body)),
+        catchError(() => EMPTY)
+      )
+    )
+  ));
+}
