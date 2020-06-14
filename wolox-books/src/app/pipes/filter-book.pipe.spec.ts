8520bfa4fe7ceb0e964614a65af1cc89b87a9503
Jorge Diaz <jorge.diaz@wolox.co>
2020-06-14 17:27:43 -0500

    Create filter book pipe

---
+++ wolox-books/src/app/pipes/filter-book.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterBookPipe } from './filter-book.pipe';
+
+describe('FilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterBookPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
